cbf7c2a6cbc49ad4ff92a66c43be3e265ccb0e4f
txtien <tranxuantien8414@gmail.com>
2020-01-06 16:27:50 +0700

    second

---
+++ flask/app/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for
+from blueprints import *
+import cv2
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.secret_key = "feij134#$qaf3%QERFAF"  # Flask secret key
+
+app.register_blueprint(home)  # blueprint for Home
+app.register_blueprint(upload_api)  # blueprint of predicting with model
+
+UPLOAD_FOLDER = 'upload'  # Save images in folder upload
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}  # Only accept two types of image extension
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
